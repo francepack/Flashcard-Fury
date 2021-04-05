77d195ca808816c890ce7d84f1a36e16ff782632
Mason France <masonrf1@gmail.com>
2021-04-05 07:33:18 -0600

    Add to utils to remove html tags from answers and provide a default for invalid questions

--- src/utils/formatClues.js
+++ src/utils/formatClues.js
@@ -1,13 +1,20 @@
 export const formatClues = (clueData) => {
+  console.log(clueData)
   let cleanedClues = [];
   clueData.forEach(clue => {
     cleanedClues.push({
-      answer: clue.answer,
+      answer: cleanAnswer(clue.answer),
       category: clue.category.title,
       hasAnsweredCorrectly: false,
       id: clue.id,
-      question: clue.question
+      question: clue.question || "API question format inconsistency"
+      // Sometimes, invalid questions are gathered from this API :(
     });
   });
   return cleanedClues;
+}
+
+// Some answers are enclosed with <i> tags which we remove to avoid printing
+const cleanAnswer = (answer) => {
+  return answer.replace( /(<([^>]+)>)/ig, '')
 }
