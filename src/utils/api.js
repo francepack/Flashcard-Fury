1f91fedfbb01c21904e80fe1b3786ef85063ac72
Mason France <masonrf1@gmail.com>
2021-04-05 23:11:59 -0600

    Adjust api calls to get more varied categories

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,28 +1,45 @@
+// fetch 6 categories, then get questions based on those categories
 export const fetchGameData = async () => {
   const categories = await fetchCategories();
   let gameQuestions = [];
   await Promise.all(
     categories.map(async category => {
-      let response = await fetch(`https://jservice.io/api/clues?category=${category.id}`)
+      let response = await fetch(`https://jservice.io/api/clues?category=${category.id}`);
       response = await response.json();
       response.forEach((question, i) => {
         if (i <= 4) {
-          gameQuestions.push(question)
+          gameQuestions.push(question);
         }
       });
-    }))
-  return gameQuestions;
+    }));
+// randomize questions - turn off for Qs of same category all together.
+  return shuffleQuestions(gameQuestions);
 }
 
 const fetchCategories = async () => {
-  let response = await fetch("https://jservice.io/api/categories?count=6");
+  const randomOffset = Math.random() * (29) + 1;
+  let response = await fetch(`https://jservice.io/api/categories?count=6&offset=${randomOffset}`);
   response = await response.json();
-  console.log(response)
   return response;
 }
 
+function shuffleQuestions(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+
 // Not it use - fetches 30 random questions
-// Switched to gather 6 categories, then get questions from those categories
 
 // export const fetchClues = async () => {
 //   let response = await fetch("https://jservice.io/api/random?count=30");
